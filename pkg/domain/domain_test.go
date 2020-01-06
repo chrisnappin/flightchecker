f667923467b152fd30c504547fc8ef76472a43a2
chris.nappin <chris@nappin.com>
2020-01-06 20:18:50 +0000

    Fixed map ordering tests

--- pkg/domain/domain_test.go
+++ pkg/domain/domain_test.go
@@ -31,7 +31,7 @@ func TestAirportMapFilter_Matching(t *testing.T) {
 	result := AirportMapFilter(dummyAirports, func(airport Airport) bool {
 		return airport.IataCode == airport2.IataCode
 	})
-	assert.Equal(t, result, expected, "Wrong result")
+	assert.EqualValues(t, result, expected, "Wrong result")
 }
 
 // TestAirportMapFilter_NotMatching tests filter when an entry is not found.
@@ -40,14 +40,14 @@ func TestAirportMapFilter_NotMatching(t *testing.T) {
 	result := AirportMapFilter(dummyAirports, func(airport Airport) bool {
 		return airport.IataCode == "XYZ"
 	})
-	assert.Equal(t, result, expected, "Wrong result")
+	assert.EqualValues(t, result, expected, "Wrong result")
 }
 
 // TestAirportMapValues_Populated tests when entries are populated.
 func TestAirportMapValues_Populated(t *testing.T) {
 	expected := []Airport{airport1, airport2}
 	result := AirportMapValues(dummyAirports)
-	assert.Equal(t, result, expected, "Wrong result")
+	assert.EqualValues(t, result, expected, "Wrong result")
 }
 
 // TestAirportMapValues_Empty tests when entries are not populated.
@@ -55,5 +55,5 @@ func TestAirportMapValues_Empty(t *testing.T) {
 	input := make(map[string]Airport)
 	expected := []Airport{}
 	result := AirportMapValues(input)
-	assert.Equal(t, result, expected, "Wrong result")
+	assert.EqualValues(t, result, expected, "Wrong result")
 }
